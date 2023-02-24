30aa7150878a680bcbf195e3c14d69559d4876b6
purplelemons-dev <ironbreaker2005@gmail.com>
2023-02-24 00:53:21 -0600

    isPrime and rsa start

---
+++ rsa/rsa_test.py
@@ -0,0 +1,23 @@
+
+# NOTES:
+# . p and q are primes
+# . n = p * q
+# . phi = (p-1) * (q-1)
+# . e, the public exponent, is a number such that
+#       1 < e < (p-1)*(q-1) and e is coprime to (p-1)*(q-1) and p*q
+# . public key is
+#      <message>^e (mod n)
+# . d, the private exponent, is a number such that
+#       d*e = 1 (mod (p-1)*(q-1))
+
+def isPrime(i:int):
+    if not i%2: return False, 2, i
+    n=3
+    while n<sqrt(i):
+        if not i%n:
+            return False, n, i
+        n+=2
+    return True, None, i
+
+def keygen(bits=2048,expo=65537)->tuple[int,int]:
+    ...
